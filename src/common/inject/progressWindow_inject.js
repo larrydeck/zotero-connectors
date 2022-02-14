68d60d56b9383ba0112dfb8518cbf15cae1e478d
Adomas Venčkauskas <adomas.ven@gmail.com>
2022-02-14 13:45:34 +0200

    Add additional style overrides for progress window
    So sites like https://tspace.library.utoronto.ca/handle/1807/104461
    don't break it

--- src/common/inject/progressWindow_inject.js
+++ src/common/inject/progressWindow_inject.js
@@ -267,6 +267,8 @@ if (isTopWindow || Zotero.isBookmarklet) {
 			maxWidth: '95%',
 			height: '120px',
 			border: "none",
+			padding: "none",
+			margin: "initial",
 			zIndex: 2147483647,
 			display: 'none'
 		};
