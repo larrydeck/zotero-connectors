c2d4ba1bd9a10c008d2786e8f6958f14bb8e6c2e
Dan Stillman <dstillman@zotero.org>
2022-06-10 02:46:04 -0400

    Enable Google Docs API pref by default
    And call it "NEW" instead of "BETA" in the prefs

--- src/common/zotero.js
+++ src/common/zotero.js
@@ -430,7 +430,7 @@ Zotero.Prefs = new function() {
 		"proxies.clientChecked": false,
 		
 		"integration.googleDocs.enabled": true,
-		"integration.googleDocs.useGoogleDocsAPI": false,
+		"integration.googleDocs.useGoogleDocsAPI": true,
 		
 		"shortcuts.cite": {ctrlKey: true, altKey: true, key: 'c'}
 	};
